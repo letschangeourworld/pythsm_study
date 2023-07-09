ee10c36c133bf4bcd6f5085447d5e44b733ef6f5
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-07-09 09:36:17 +0900

    Update flutter(flow)_01.dart

--- quickapp/flutter(flow)_01.dart
+++ quickapp/flutter(flow)_01.dart
@@ -1,6 +1,4 @@
-
 /*
-
 따라하기 강의
 https://m.blog.naver.com/rock11201/222879641299
 
@@ -8,18 +6,20 @@ https://m.blog.naver.com/rock11201/222879641299
 https://flutterflow.io/
 
 FlutterFlow로 만든 앱 화면을 App Store에 게시하고 
-서비스하기 위해서는 Xcode를 사용하여 앱을 빌드해야 합니다. 
+서비스하기 위해서는 Xcode를 사용하여 앱을 빌드해야 한다.
 
-1. 우선 Mac OS용 컴퓨터와 Xcode를 설치합니다.
-2. FlutterFlow에서 앱을 빌드하기 위해 Flutter SDK를 설치합니다.
-3. FlutterFlow에서 에셋을 준비합니다.
-4. Xcode에서 새로운 프로젝트를 만듭니다.
-5. Flutter 앱을 Xcode 프로젝트에 추가합니다.
-6. Flutter 앱의 정보를 업데이트하고 그림자와 텍스트 스타일 등 UI 디자인을 변경합니다.
-7. 최종 앱을 빌드합니다.
-8. Developer Program의 일부인 앱 스토어 Connect에서 앱을 업로드하고, 검토 및 승인을 받습니다. 
+1. 우선 Mac OS용 컴퓨터와 Xcode를 설치
+2. FlutterFlow에서 앱을 빌드하기 위해 Flutter SDK를 설치
+3. FlutterFlow에서 에셋을 준비
+4. Xcode에서 새로운 프로젝트를 만듬
+5. Flutter 앱을 Xcode 프로젝트에 추가
+6. Flutter 앱의 정보를 업데이트하고 그림자와 텍스트 스타일 등 UI 디자인을 변경
+7. 최종 앱을 빌드
+8. Developer Program의 일부인 앱 스토어 Connect에서 앱을 업로드하고,
+   검토 및 승인을 받음
 
-위와 같은 과정을 거쳐 FlutterFlow로 만든 앱 화면을 App Store에서 서비스할 수 있습니다.
+위와 같은 과정을 거쳐 FlutterFlow로 만든 앱 화면을 
+App Store에서 서비스할 수 있슴
 */
 
 import 'package:flutter/material.dart';
@@ -64,11 +64,12 @@ class MyApp extends StatelessWidget {
 
 /*
 위 코드는 화면 중앙에 이름과 이메일을 입력할 수 있는 두 개의 텍스트 필드와, 
-입력한 정보를 저장할 수 있는 저장 버튼이 있는 간단한 화면입니다.
+입력한 정보를 저장할 수 있는 저장 버튼이 있는 간단한 화면이다.
 코드 상에서 `TextField` 위젯을 사용하여 입력 필드를 생성하고, 
-`ElevatedButton` 위젯을 사용하여 저장 버튼을 생성합니다.
+`ElevatedButton` 위젯을 사용하여 저장 버튼을 생성한다.
 버튼 클릭 시, onPressed 콜백 함수를 통해 입력한 정보를 저장하거나
-다른 작업을 수행할 수 있습니다. 이 코드를 참고하여 원하는 정보 입력 화면을 구성하고 
-필요한 코드를 작성하시면 됩니다.
+다른 작업을 수행할 수 있다. 
+이 코드를 참고하여 원하는 정보 입력 화면을 구성하고 
+필요한 코드를 작성하면 된다.
 */
 
