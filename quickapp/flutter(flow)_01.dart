bf61516fafb6b74ab986c3f8818e993b8224b2df
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-07-17 07:51:46 +0900

    Update flutter(flow)_01.dart

--- quickapp/flutter(flow)_01.dart
+++ quickapp/flutter(flow)_01.dart
@@ -1,29 +1,3 @@
-/*
-[flutterflow 따라하기 강의]
-   https://m.blog.naver.com/rock11201/222879641299
-[flutterflow 사이트]
-   https://flutterflow.io/
-
-   https://thoughtful-tarsier-267.notion.site/f408ac46e2c34cd981a1b2e3033aef98
-
-FlutterFlow로 만든 앱 화면을 
-App Store에 게시하고 서비스하기 위해서는
-Xcode를 사용하여 앱을 빌드해야 한다.
-
-1. 우선 Mac OS용 컴퓨터와 Xcode를 설치
-2. FlutterFlow에서 앱을 빌드하기 위해 Flutter SDK를 설치
-3. FlutterFlow에서 에셋을 준비
-4. Xcode에서 새로운 프로젝트를 만듬
-5. Flutter 앱을 Xcode 프로젝트에 추가
-6. Flutter 앱의 정보를 업데이트하고 그림자와 텍스트 스타일 등 UI 디자인을 변경
-7. 최종 앱을 빌드
-8. Developer Program의 일부인 앱 스토어 Connect에서 앱을 업로드하고,
-   검토 및 승인을 받음
-
-위와 같은 과정을 거쳐 FlutterFlow로 만든 앱 화면을 
-App Store에서 서비스할 수 있슴
-*/
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
