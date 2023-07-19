0974b54d2230cad9ac703a206bba01d0564d9273
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-07-19 21:05:55 +0900

    Update start.dart

--- quick_dart/start.dart
+++ quick_dart/start.dart
@@ -1,1 +1,17 @@
+// 1. 기본 개념
 
+// 모든 dart 프로그램은 아래와 같이 main 메서드(method)로 실행시작된다.
+// 메서드는 기능을 실행하는 코드뭉치를 말한다.
+
+void main() {
+    print("Hello World");
+}
+
+// 주석(comment or annotation)은 실행되지 않는다
+
+// 2. 데이터 타입
+
+/*
+
+
+*/
