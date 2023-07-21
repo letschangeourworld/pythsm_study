bdc4259f2774d68c52cdd62a57bd7f1e167564ff
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-07-22 01:14:07 +0900

    Create 17_switch_statement.dart

---
+++ quick_dart/basic/17_switch_statement.dart
@@ -0,0 +1,17 @@
+// Switch Statement
+
+void main() {
+  int num = 19;
+
+  switch (num % 2) {
+    case 0:
+      print('2');
+      break;
+    case 1:
+      print('3');
+      break;
+    default:
+      print('No!');
+      break;
+  }
+}
