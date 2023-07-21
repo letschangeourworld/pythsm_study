58878aff064d4bbd02c77696f857400234ad2245
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-07-22 01:33:42 +0900

    Create 24_class_definition.dart

---
+++ quick_dart/basic/24_class_definition.dart
@@ -0,0 +1,16 @@
+// class definition
+
+class Fruit {
+  String name = 'Apple';
+
+  void printName() {
+    print('My name is ${this.name}!');
+  }
+}
+
+void main() {
+  Fruit fruit = new Fruit();
+  fruit.printName();
+  fruit.name = 'Banana';
+  fruit.printName();
+}
