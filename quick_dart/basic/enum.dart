6d7e667d1fff8350761908700937b0bbd3ee6a95
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-07-22 00:31:11 +0900

    Create enum.dart

---
+++ quick_dart/basic/enum.dart
@@ -0,0 +1,13 @@
+// enum
+
+enum Status {
+  wait,
+  approved,
+  reject,
+}
+
+void main() {
+  Status currentStatus = Status.wait;
+  print(currentStatus == Status.approved);
+  print(Status.values.toList());
+}
