d39d7cce50f820135a7438c2016e6b1708f78fd1
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-07-22 01:19:10 +0900

    Create 19_dowhile_statement.dart

---
+++ quick_dart/basic/19_dowhile_statement.dart
@@ -0,0 +1,33 @@
+// do while Statement
+
+void main() {
+  int i = 0;
+  while (i < 10) {
+    print(i);
+    i++;
+  }
+
+  i = 0;
+  do {
+    print(i);
+    i++;
+  } while (i < 10);
+
+  i = 0;
+  while (true) {
+    print(i);
+    i++;
+    if (i == 5) {
+      break;
+    }
+  }
+
+  i = 0;
+  while (i < 10) {
+    if (i == 5) {
+      continue;
+    }
+    print(i);
+    i++;
+  }
+}
