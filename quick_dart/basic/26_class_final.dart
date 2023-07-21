2aa5fe767a3c312bf6c207ea70ffa29e5cb9b11f
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-07-22 07:00:34 +0900

    Create 26_class_final.dart

---
+++ quick_dart/basic/26_class_final.dart
@@ -0,0 +1,21 @@
+// final member variable in class
+
+class Fruit {
+  final String? name;
+  final String? color;
+
+  Fruit({String? name, String? color})
+      : this.name = name,
+        this.color = color;
+
+  void printName() {
+    print('My name is ${this.name}(${this.color})!');
+  }
+}
+
+void main() {
+  Fruit fruit = new Fruit(name: 'Apple', color: 'Red');
+  fruit.printName();
+
+  // fruit.name = 'Kiwi'; // << ERROR
+}
