cdb64ad6ec60b20bafd908fa07de86dc4a902f0f
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-07-22 07:19:15 +0900

    Create 31_class_static_variable.dart

---
+++ quick_dart/basic/31_class_static_variable.dart
@@ -0,0 +1,29 @@
+// Staric member variable in class
+
+class Food {
+  static String? kind;
+  String? name;
+
+  Food(String name) : this.name = name;
+
+  static printAll(String name, String kind) {
+    print('${name} is ${kind}!');
+  }
+
+  void printName() {
+    print('My name is ${this.name}! I am ${kind}!');
+  }
+}
+
+void main() {
+  Food apple = new Food('Apple');
+  apple.printName();
+  Food banana = new Food('Banana');
+  banana.printName();
+
+  Food.kind = 'Fruit';
+  apple.printName();
+  banana.printName();
+
+  Food.printAll('Apple', 'Red Fruit');
+}
