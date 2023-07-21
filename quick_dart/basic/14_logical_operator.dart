863fcd2dd5c8116740ebe707ccc67ea43038b56f
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-07-22 01:06:23 +0900

    Create 14_logical_operator.dart

---
+++ quick_dart/basic/14_logical_operator.dart
@@ -0,0 +1,14 @@
+// logical operator
+
+void main() {
+  print(true && true);
+  print(true && false);
+  print(false && true);
+  print(false && false);
+  print(true || true);
+  print(true || false);
+  print(false || true);
+  print(false || false);
+  print(!true);
+  print(!false);
+}
