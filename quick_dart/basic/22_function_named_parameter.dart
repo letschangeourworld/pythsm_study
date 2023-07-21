df537d2c90f101e3e491f8be2aff18e7152798f6
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-07-22 01:28:47 +0900

    Create 22_function_named_parameter.dart

---
+++ quick_dart/basic/22_function_named_parameter.dart
@@ -0,0 +1,13 @@
+// Named Parameter in function
+
+String add(int a, int b, {int c = 0, int d = 0}) {
+  int sum = a + b + c + d;
+  return 'Sum: $sum';
+}
+
+void main() {
+  print(add(1, 1));
+  print(add(1, 1, c: 1));
+  print(add(1, 1, d: 1));
+  print(add(1, 1, c: 1, d: 1));
+}
