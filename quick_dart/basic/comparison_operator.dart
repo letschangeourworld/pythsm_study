b4532a95437a8a175d5bb4cdaeedee97750d0764
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-07-22 00:51:25 +0900

    Create comparison_operator.dart

---
+++ quick_dart/basic/comparison_operator.dart
@@ -0,0 +1,13 @@
+// comparison operator
+
+void main() {
+  int num1 = 3;
+  int num2 = 5;
+
+  print(num1 > num2);
+  print(num1 < num2);
+  print(num1 >= num2);
+  print(num1 <= num2);
+  print(num1 == num2);
+  print(num1 != num2);
+}
