cafb9e94cff6ef830b442bec834709ad697ee4cd
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-07-27 15:42:23 +0900

    Update flutter_04_BoxBar.dart

--- quickapp/flutter_04_BoxBar.dart
+++ quickapp/flutter_04_BoxBar.dart
@@ -1,57 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const MyApp()); // const
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);  // null check for Key
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'AnyLike',
+      debugShowCheckedModeBanner: false,  // remove upper banner
+      title: 'AnyLike',  // title of window
       theme: ThemeData(
-        primarySwatch: Colors.cyan,
-      ),
-      home: const MyHomePage(title: 'AnyLike Product'),
-    );
+        primarySwatch: Colors.cyan, // appbar color
+      ), // ThemData
+      home: const MyHomePage(), // const
+    ); // MaterialApp
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, this.title}) : super(key: key);
-
-  final String? title;
-
+  const MyHomePage({Key? key, this.title}) : super(key: key); // null check for Key
+  
+  final String? title; // null check
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("AnyLike Products")),
+      appBar: AppBar(title: const Text("AnyLike Product")), // const
       body: ListView(
         shrinkWrap: true,
-        padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
-        children: const <Widget>[
+        padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0), // const
+        children: const <Widget>[ // const, Widget <- W UpperLetter
           ProductBox(
             name: 'iPhone',
             price: 1000,
             image: "iphone.jpg"
-          ),
-        ],
-      ),
-    );
+          ), // ProductBox
+        ], // <Widget>[]
+      ), // ListView
+    ); // Scaffold
   }
 }
 
 class ProductBox extends StatelessWidget {
+  // null check : ? and required
   const ProductBox({Key? key, this.name, this.price, required this.image}) : super(key: key);
-  final String? name;
-  final int? price;
+  final String? name; // null check
+  final int? price; // null check
   final String image;
-
+  
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(2),
+      padding: const EdgeInsets.all(2), // const
       height: 120,
       child: Card(
         child: Row(
@@ -59,23 +60,23 @@ class ProductBox extends StatelessWidget {
           children: <Widget>[
             Image.asset('productImage/$image'),
             Expanded(child: Container(
-              padding: const EdgeInsets.all(5),
+              padding: const EdgeInsets.all(5), // const
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   Text(
-                      name!,
-                  style: const TextStyle(fontWeight: FontWeight.bold)
+                      name!, // null check operator
+                  style: const TextStyle(fontWeight: FontWeight.bold) // const
                   ),
                 Text("Price: $price USD"),
-                ],
-              )
-            )
-           )
-          ],
-        ),
-      ),
-    );
+                ], // <Widget>[]
+              ) // Column
+            ) // Container
+           ) // Expanded
+          ], // <Widget>[]
+        ), // Row
+      ), // Card
+    ); // Container
   }
 }
 
