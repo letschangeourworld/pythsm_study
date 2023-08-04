4c1ea7a0b9c1bd289878d5ff1cefb3f201fe8d30
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-08-04 13:37:16 +0900

    Create note.dart

---
+++ quick_dart/studyNote/note.dart
@@ -0,0 +1,40 @@
+
+1. Flutter Installation -> C drive
+   Dart-sdk download and installation if needed
+2. Configuration Check with flutter doctor
+    : java latest update, VS development upgrade etc.
+3. Android Studio Installation and configuration
+    a. settings > Plugins > flutter installation and dart
+        path selection : flutter install files directory
+        path selection : dart-sdk install files directory
+    a. File > New Project > flutter or dart
+    a. Android SDK Manager
+    b. SDK Tools
+        ㄱ. Android SDK Build-Tools 34 
+        ㄴ. CMake
+        ㄷ. Android SDK Comman-line Tools
+        ㄹ. SDK Platforms : Android 11 installation
+    c. Device Manager
+        ㄱ. Create Device
+        ㄴ. Phone pixel 2 api 30 : Android 11
+
+
+
+Dart Language
+
+* class -> compile (storing to memory) -> instance (finished storing to memory)
+* variable type keyword : var,dynamic,int,String,double,bool, etc.
+* No public,protected,private keyword
+* private identifiers : use underscore(_) in front of variables.
+* debug mode and release mode
+* built-in identifiers (내장식별자, 내장함수)
+  : abstract,as,dynamic,export,factory,Function,get,import,
+    implements,static,typedef,mixin,set, etc.
+* Contextual keyword (문맥적 키워드) : sync,async,hide,on,show
+* 2 keywords can't be used as an identifier in 'async','async*','sync*' methods
+  : await, yield
+* reserved words
+
+
+
+    
