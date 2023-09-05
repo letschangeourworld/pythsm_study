bb9f43147e89b6b3297c37073fac460627439770
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-09-05 17:44:12 +0900

    Update DownloadYoutube.py

--- VideoShow/DownloadYoutube.py
+++ VideoShow/DownloadYoutube.py
@@ -2,7 +2,18 @@
 youtube_dl 라이브러리 안의 extractor 폴더의 youtube.py 프로그램을 수정해줘야 한다.
 YoutubeVideo.py 파일을 확인해 볼 것
 '''
-
+'''
+title : 비디오 제목
+url : 비디오 URL
+ext : 비디오 파일 확장자
+uploader : 비디오 업로더
+upload_date : 비디오 업로드 날짜 (YYYYMMDD)
+comment_count 해당 비디오 코멘트 숫자
+width : 비디오 width
+height : 비디오 height
+playlist_index : 플레이 리스트 인덱스
+playlist_title (string) : 플레이 리스트 제목
+'''
 
 import youtube_dl
 import os
