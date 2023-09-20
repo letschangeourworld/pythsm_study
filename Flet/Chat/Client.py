2a662fff17e76fd993a28719a30e71c0528de302
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-09-20 15:12:28 +0900

    Update Client.py

--- Flet/Chat/Client.py
+++ Flet/Chat/Client.py
@@ -6,7 +6,7 @@ from ChatApp import Message, ChatMessage
 from flask import Flask
 from flask_cors import CORS
 
-#localhost=127.0.0.1 et port 3000; IPV4= 192.168.0.--- port 5000
+# localhost=127.0.0.1 et port 3000; IPV4= 192.168.0.--- port 5000
 HOST = "192.168.0.---"
 PORT = 3000
 DEFAULT_FLET_PATH = 'http:/localhost'  # or 'ui/path'
@@ -15,33 +15,29 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
-#main fonction
 def main(page: ft.Page):
     print("main")
-    #création object socket
+    # 클라이언트 소켓 생성
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    page.title = "You Enjoy Mychatbot"
-    page.add(ft.Text("connecter avec succès"))
-
-    #connect au serveur
+    page.title = "Chat Window"
+    page.add(ft.Text("Connection"))
+    
+    # Host 연결
     try:
         client.connect((HOST, PORT))
-        print(f"Successfully connected to server")
-        n_client =client.recv(1024)
+        print(f"Connected to Host Server Successfully")
+        n_client = client.recv(1024)
         print(n_client.decode())
         while n_client:
-            message = input("Entrez votre message")
-            
+            message = input("Inpu Message")
             client.send(message.encode())
-           
             n_client =client.recv(1024)
             print(n_client.decode())
     except:
         print(f"Unable to connect to server {HOST}{PORT}")
 
 if __name__=="__main__":
-    #main()
     flet_path = os.getenv("FLET_PATH", DEFAULT_FLET_PATH)
     flet_port = int(os.getenv("FLET_PORT", DEFAULT_FLET_PORT))
-    ft.app(target=main, view=ft.WEB_BROWSER)
+    ft.app(target = main, view = ft.WEB_BROWSER)
     app.run(host = '192.168.0.---', port = 3000)
