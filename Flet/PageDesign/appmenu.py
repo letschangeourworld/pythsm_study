d73a043482a32630a7c649db8282ea48081b076d
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-09-21 19:48:00 +0900

    Update appmenu.py

--- Flet/PageDesign/appmenu.py
+++ Flet/PageDesign/appmenu.py
@@ -12,16 +12,20 @@ import flet as ft
 # Text     : (text_data,weight,size,color)
 # Row      : ([list_data],scroll,alignment)
 
-appmenu = ft.Row([
-    ft.CircleAvatar(
-        foreground_image_url = "https://image2.1004gundam.com/item_images/goods/380/1376395068.jpg"),
-    ft.Container(
-        margin = ft.margin.symmetric(vertical = 40),
-        content = ft.Row([
-            ft.Icon(name = 'pin_drop', color = 'white'),
-            ft.Text('Seoul City', color = 'white'),
-            ft.Icon(name = 'search', color = 'white')
-        ])
-        )
-    ]
+# 아바타 프로필에 들어갈 이미지
+foreground_image_url = "https://image2.1004gundam.com/item_images/goods/380/1376395068.jpg"
+
+# Row 생성하여 그 안에 Container 넣기
+# Container 안에 Row 생성하여 그 안에 Icon 3개(pin_drop,Seoul City,search) 넣기
+appmenu = ft.Row(
+    [ft.CircleAvatar(foreground_image_url),
+     ft.Container(margin = ft.margin.symmetric(vertical = 40),
+                  content = ft.Row(
+                      [ft.Icon(name = 'pin_drop', color = 'white'),
+                       ft.Text('Seoul City', color = 'white'),
+                       ft.Icon(name = 'search', color = 'white')
+                       ]
+                  )
+                  )
+     ]
     )
