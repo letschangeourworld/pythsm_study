96ab25867178e18f0173ae098540879e84201b1c
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-09-21 20:04:21 +0900

    Update Gridcard.py

--- Flet/PageDesign/Gridcard.py
+++ Flet/PageDesign/Gridcard.py
@@ -1,15 +1,6 @@
 import flet as ft
 from Moviedata import grid_list_image
 
-gridcom = ft.GridView(
-    expand = 3,
-    runs_count = 5,
-    max_extent = 150,
-    child_aspect_ratio = 1,
-    spacing = 15,
-    run_spacing = 15
-    )
-
 # controls : controlling the data
 # append   : adding the data to the list repeatedly
 # Card : the fixed form to insert onto the rectangle Grid
@@ -22,27 +13,38 @@ gridcom = ft.GridView(
 # Text     : (text_data,weight,size,color)
 # Row      : ([list_data],scroll)
 
-# To insert the images to Grid
+# 그리드 틀 만들기
+gridcom = ft.GridView(expand = 3,
+                      runs_count = 5,
+                      max_extent = 150,
+                      child_aspect_ratio = 1,
+                      spacing = 15,
+                      run_spacing = 15
+                      )
+
+# 그리드 틀 안에 연속으로 이미지 삽입
+# 그리드 틀의 각 칸에 Card 형태로 삽입
+# Card 안에 지정된 주소의 이미지를 불러와 이미지 삽입
 for x in grid_list_image:
     gridcom.controls.append(
-        ft.Card(
-            elevation = 20,
-            content = ft.Image(
-                src = x,
-                border_radius = ft.border_radius.all(10),
-                fit = ft.ImageFit.COVER
+        ft.Card(elevation = 20,
+                content = ft.Image(
+                    src = x,
+                    border_radius = ft.border_radius.all(10),
+                    fit = ft.ImageFit.COVER )
                 )
-            )
         )
 
-gridscreen = ft.Container(
-    margin = ft.margin.only(left = 20),
-    content = ft.Column(
-        [ft.Text('Now Showing',
-              weight = 'bold',
-              size = 30,
-              color = 'white'),
-         gridcom],
-        scroll = 'auto'
-        )
-    )
+# Container 생성
+# Container 안에 Column 삽입
+# Column 안에 Text 넣고 그 아래 gridcom 삽입
+gridscreen = ft.Container(margin = ft.margin.only(left = 20),
+                          content = ft.Column(
+                              [ft.Text('Now Showing',
+                                       weight = 'bold',
+                                       size = 30,
+                                       color = 'white'
+                                      ),
+                               gridcom],
+                              scroll = 'auto')
+                          )
