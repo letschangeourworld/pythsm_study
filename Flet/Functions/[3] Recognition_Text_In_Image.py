5dfff74de13ccc73572d891190b15318e8bd410f
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-10-02 15:14:20 +0900

    Update [3] Recognition_Text_In_Image.py

--- Flet/Functions/[3] Recognition_Text_In_Image.py
+++ Flet/Functions/[3] Recognition_Text_In_Image.py
@@ -1,16 +1,23 @@
 import flet as ft
 from flet import *
-import pytesseract
+import pytesseract      # Need to install pytesseract
 from PIL import Image
 import os
+
+# Make a virtual environment based on python and install pytesseract through pip
+# Download pytesseract installation file in this site below :
+# https://github.com/UB-Mannheim/tesseract/wiki
+# Select your own language in Additional language/script data during installation
+# Add the path of pytesseract directory installed into the environment variable of your pc
+# Force to designate the directory of pytesseract execution as follows
 
 pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
 
 def main(page: ft.Page):
     page.scroll = "auto"
-    pic_loc = ft.TextField(label = "Write down the name of image file")
+    pic_loc = ft.TextField(label = "Fill in the image file name (ex: example.png)")
     pic_preview = ft.Image(src = False, width = 500, height = 500)
-
+    
     def process_pic(e):
         pic_pro = Image.open(pic_loc.value)
         txt = pytesseract.image_to_string(pic_pro)
