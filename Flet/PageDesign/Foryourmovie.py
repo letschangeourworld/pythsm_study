7c6b92589c416dcd621d13a4f445dee077ef78d1
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-10-04 09:19:28 +0900

    Update Foryourmovie.py

--- Flet/PageDesign/Foryourmovie.py
+++ Flet/PageDesign/Foryourmovie.py
@@ -2,21 +2,28 @@ import flet as ft
 from Moviedata import movie_data
 
 # To deliver the movie data into the specific functions
-# controls : controlling the data
+# controls : controlling the data                      ---> flet클래스들의 인수, list 데이터 받음 
 # append   : adding the data to the list repeatedly
-# stack    : laying a Container upon another
+# stack    : laying a Container upon another           ---> ft.Stack(). flet의 클래스, list 데이터 받음 
 # Container : margin,width,height,border_radius,padding,bottom
-#             gradient,content
-# gradient : LinearGradient(begin,end,colors)
-# content  : Column([Text, Row],spacing,scroll),
-#            Image(src,width,height,border_radius,fit)
-# Text     : (text_data,weight,size,color)
-# Row      : ([list_data],scroll)
+#             gradient,content                         
+#             ---> ft.Container(), flet의 클래스
+#             ---> ft.border_radius, flet의 함수
 
-# listmovie라는 Row 형태의 빈 list틀 생성
+# gradient : LinearGradient(begin,end,colors)          ---> ft.LinearGradient(), flet의 함수
+# content  : Column([Text, Row],spacing,scroll),       ---> ft.Column(), flet의 클래스
+#            Image(src,width,height,border_radius,fit) ---> ft.Image(), flet의 클래스
+#            ---> ft.border_radius, flet의 함수
+#            ---> ft.ImageFit, flet의 클래스
+# Text     : (text_data,weight,size,color)             ---> ft.Text(), flet의 클래스
+# Row      : ([list_data],scroll)                      ---> ft.Row(), flet의 클래스
+
+# listmovie라는 변수를 ft.Row 틀로 만들어 선언함 (빈 형식틀)
 listmovie = ft.Row()
 
 # listmovie 안에 연속으로 Stack 안에 Container 쌓기
+# listmovie 는 ft.Row 형식이므로 ft.Row() 클래스함수 안의 controls인수를 사용하여 데이터 control함
+# controls인수는 [], 즉 리스트 형식으로 데이터를 받음
 # Container 안에 연속으로 이미지 넣기
 # 이미지가 없는 Container는 그 안에 Column을 넣기
 for x in movie_data:
