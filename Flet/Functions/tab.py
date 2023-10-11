b1fb4b571fe94acecf34338801b051b033bfce8e
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-10-11 19:27:28 +0900

    Create tab.py

---
+++ Flet/Functions/tab.py
@@ -0,0 +1,83 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.fonts = {
+        "HDSansText":"C:\\Users\\2801123\\AppData\\Local\\Microsoft\\Windows\\Fonts\\HYUNDAISANSTEXTKROTFREGULAR.OTF"
+    }
+    page.title = "Paint Manufacturing Engineering Team 1"
+    page.window_width = 1160
+    page.window_height = 700
+    page.padding = 5
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.scroll = "always"
+    # page.vertical_alignment = ft.MainAxisAlignment.END
+    # page.horizontal_alignment = ft.MainAxisAlignment.END
+  
+    # Appbar 기능
+    page.appbar = ft.AppBar(
+        leading = ft.Icon(
+            ft.icons.EDIT_SQUARE,
+            color = ft.colors.BLUE,
+            size = 30 ),
+        leading_width = 50,
+        title = ft.Text(
+            "MH Editor",
+            size = 22,
+            weight = ft.FontWeight.BOLD,
+            color = ft.colors.BLACK,
+            font_family = "HDSansText"
+        ),
+        center_title = False,
+        bgcolor = ft.colors.SURFACE_VARIANT
+    )
+    
+    t = ft.Tabs(
+        selected_index = 0,
+        animation_duration = 400,
+        tabs = [
+            ft.Tab(
+                tab_content = ft.Row(
+                    [
+                        ft.Icon(ft.icons.DRIVE_FILE_RENAME_OUTLINE_SHARP),
+                        ft.Text("Standard Sheet",
+                                font_family = "HDSansText",
+                                size = 15
+                                )
+                    ]
+                ),
+                content = ft.Container(
+                    content = ft.Text("This is TAB 1"),
+                    alignment = ft.alignment.center
+                ),
+            ),
+            ft.Tab(
+                tab_content = ft.Row(
+                    [
+                        ft.Icon(ft.icons.DRIVE_FILE_RENAME_OUTLINE_ROUNDED),
+                        ft.Text("Paint Area",
+                                font_family = "HDSansText",
+                                size = 15
+                                )
+                    ]
+                ),
+                content = ft.Text("This is TAB 2")
+            ),
+            ft.Tab(
+                tab_content = ft.Row(
+                    [
+                        ft.Icon(ft.icons.CALCULATE),
+                        ft.Text("MH Calculator",
+                                font_family = "HDSansText",
+                                size = 15
+                                )
+                    ]
+                ),
+                content = ft.Text("This is TAB 3")
+            )
+        ],
+        # expand = 1,
+        divider_color = ft.colors.BLUE_GREY_100
+    )
+    page.add(t)
+
+ft.app(target = main)
