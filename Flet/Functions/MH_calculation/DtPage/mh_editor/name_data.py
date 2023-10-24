8b33730ab20ecd744b97b1e31c4d8652933813c6
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-10-24 18:35:45 +0900

    Create name_data.py

---
+++ Flet/Functions/MH_calculation/DtPage/mh_editor/name_data.py
@@ -0,0 +1,36 @@
+data01 = {
+        "NO": "1",
+        "INSTRUCTION" : '루프패드를 잡는다.',
+        "LENGTH" : '1',
+        "WIDTH" : '1',
+        "AREA" : '1',
+        "Q'TY" : '2',
+        "SYMBOL" : 'M04G04',
+        "MOD" : '7.0',
+        "MH": '2.0'
+    }
+
+plant_name = {
+    "" : 10,
+    "" : 11,
+    "" : 12,
+    "" : 20,
+    "" : 30,
+}
+
+process_name = {
+    
+}
+
+car_model_name = {
+    
+}
+
+font_path = {
+    "HDText" : "C:\\Windows\\Fonts\\HyundaiSansTextKRRegular.ttf"
+}
+
+files = {
+    "instruction": "mh_instruction_sheet.xlsx",
+    "area" : "mh_part_area_sheet.xlsx"
+}
