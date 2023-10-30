3b103612ff16b1bb5fd40c5fc46fc97524d93846
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-10-30 17:37:06 +0900

    Update tab_in_std.py

--- Flet/Functions/MH_calculation/DtPage/mh_editor/tab_in_std.py
+++ Flet/Functions/MH_calculation/DtPage/mh_editor/tab_in_std.py
@@ -7,34 +7,34 @@ from dataframe import *
 
 def std_tab_menu(
     page,
-    df_name,
+    df_dict,
     plant_name,
     process_name,
     car_model_name):
     
+    # df_name : The name or path of excel files made in a dictionary
+    df_names_list = list(df_dict.values())
+    df_sht_names_list = pd.ExcelFile(df_names_list[0]).sheet_names
     
-    df_name_list = list(df_name.values())
-    df_sht_name_list = pd.ExcelFile(df_name_list[0]).sheet_names
-    
-    std_table_list = []
-    for i, tn in enumerate(df_sht_name_list):
-        df_std_sht = pd.read_excel(df_name_list[0],
-                                   sheet_name = df_sht_name_list[tn],
-                                   header = 1)
-        df_std_sht = df_std_sht.iloc[:26,2:]
-        df_std_sht = df_std_sht.fillna("")
-        df_std_sht = df_std_sht[df_std_sht.iloc[:,6] != ""]
-        std_datatable = ft.DataTable(columns = col_name(df_std_sht),
-                                     rows = rows(df_std_sht))
+    std_tables_list = []
+    for sn in df_sht_names_list:
+        std_df = pd.read_excel(df_names_list[0],
+                               sheet_name = df_sht_names_list[sn],
+                               header = 1)
+        std_df = std_df.iloc[:26,2:]
+        std_df = std_df.fillna("")
+        std_df = std_df[std_df.iloc[:,6] != ""]
+        std_datatable = ft.DataTable(columns = col_name(std_df),
+                                     rows = rows(std_df))
         std_table = StdSht(
             datatable = std_datatable,
             plant_name= plant_name,
             process_name = process_name,
             car_model_name = car_model_name)
             
-        std_table_list.append(std_table)
+        std_tables_list.append(std_table)
         
-    tab_menu_in_stdsht = ft.Tabs(
+    tab_menu_in_stdtable = ft.Tabs(
         selected_index = 0,
         scrollable = True,
         expand = True,
@@ -43,7 +43,7 @@ def std_tab_menu(
                 tab_content = ft.Row(
                     [
                         ft.Icon(ft.icons.DRIVE_FILE_RENAME_OUTLINE_SHARP),
-                        ft.Text("Standard Sheet",
+                        ft.Text(sht_name,
                                 font_family = "HDText",
                                 size = 15,
                                 weight = "bold")
@@ -51,10 +51,10 @@ def std_tab_menu(
                 ),
                 content = std_table
             )
-        ]
+        ] for std_table, sht_name in zip(std_tables_list, df_sht_names_list)
     )
     return ft.Column(
         expand = True,
         alignment = ft.MainAxisAlignment.CENTER,
-        controls = [tab_menu_in_stdsht]
+        controls = [tab_menu_in_stdtable]
     )
