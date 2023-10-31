65a495a13c7f1725ce0971f2a58e124f8cd6531a
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-10-31 18:44:05 +0900

    Create std_sht.py

---
+++ Flet/Functions/MH_calculation/DtPage/mh_editor3/std_sht.py
@@ -0,0 +1,91 @@
+import flet as ft
+
+class StdSht(ft.UserControl):
+    def __init__(self,
+                 datatable: ft.DataTable,
+                 table_title: str = "Instruction Table",
+                 process_name: str = "",
+                 plant_name: str = "",
+                 car_model_name: str = ""):
+        super().__init__()
+        
+        self.dt = datatable
+        self.title = table_title
+        self.process_name = process_name
+        self.plant_name = plant_name
+        self.car_name = car_model_name
+        self.pdt = ft.DataTable(
+            width= 1005,
+            border = ft.border.all(0.5, color = ft.colors.BLACK45),
+            border_radius = 10,
+            heading_row_color = ft.colors.BLACK26,
+            heading_row_height = 40,
+            column_spacing = 1,
+            columns = self.dt.columns,
+            rows = self.dt.rows
+        )
+    
+    def build(self):
+        one_tab = ft.Column(
+            [
+                ft.Text(""),
+                ft.Card(
+                    ft.Container(
+                        ft.Column(
+                            [
+                                ft.Row(
+                                    [
+                                        ft.Row(
+                                            [
+                                                ft.Text(self.title,
+                                                        color = ft.colors.BLACK,
+                                                        weight = "bold",
+                                                        size = 20,
+                                                        text_align = ft.TextAlign.CENTER,
+                                                        style = ft.TextThemeStyle.HEADLINE_MEDIUM,
+                                                        font_family = "HDText"),
+                                            ],
+                                            alignment = ft.MainAxisAlignment.START
+                                        ),
+                                        ft.Row(
+                                            [
+                                                ft.Text(f"| Plant : {self.plant_name} ",
+                                                        color = ft.colors.BLACK,
+                                                        weight = "bold",
+                                                        size = 15,
+                                                        text_align = ft.TextAlign.RIGHT,
+                                                        font_family = "HDText"
+                                                ),
+                                                ft.Text(f"| Process : {self.process_name} ",
+                                                        color = ft.colors.BLACK,
+                                                        weight = "bold",
+                                                        size = 15,
+                                                        text_align = ft.TextAlign.RIGHT,
+                                                        font_family = "HDText"
+                                                ),
+                                                ft.Text(f"| Car Model : {self.car_name}  ",
+                                                        color = ft.colors.BLACK,
+                                                        weight = "bold",
+                                                        size = 15,
+                                                        text_align = ft.TextAlign.RIGHT,
+                                                        font_family = "HDText"
+                                                ),
+                                            ],
+                                            alignment = ft.MainAxisAlignment.END
+                                        )
+                                    ],
+                                    alignment = ft.MainAxisAlignment.SPACE_BETWEEN
+                                ),
+                                self.pdt,
+                            ],
+                        ),
+                        padding = 25,
+                        bgcolor = ft.colors.BLUE_GREY_100,
+                        border_radius = ft.border_radius.all(10)
+                    ),
+                    elevation = 5,
+                )
+            ],
+            scroll = ft.ScrollMode.AUTO
+        )
+        return one_tab
