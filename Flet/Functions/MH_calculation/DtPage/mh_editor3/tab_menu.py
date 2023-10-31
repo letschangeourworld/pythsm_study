dd1ef698f2632457d17b4e28d10f3747b49b0a05
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-10-31 18:44:38 +0900

    Create tab_menu.py

---
+++ Flet/Functions/MH_calculation/DtPage/mh_editor3/tab_menu.py
@@ -0,0 +1,72 @@
+import flet as ft
+from paint_area import PaintArea
+import mh_calc as mc
+import tab_in_std as tis
+
+def tab_menu(page,
+             std_df_name,
+             area_df_name,
+             plant_name,
+             process_name,
+             car_model_name ):
+    
+    std_tabs = tis.std_tab_menu(
+        page = page,
+        df_name = std_df_name,
+        plant_name = plant_name,
+        process_name = process_name,
+        car_model_name = car_model_name)
+    
+    paint_table = PaintArea(
+        df_name = area_df_name,
+        car_model_name = car_model_name)
+    
+    top_menu = ft.Tabs(
+        selected_index = 0,
+        animation_duration = 300,
+        scrollable = True,
+        expand = True,
+        tabs = [
+            ft.Tab(
+                tab_content = ft.Row(
+                    [
+                        ft.Icon(ft.icons.DRIVE_FILE_RENAME_OUTLINE_SHARP),
+                        ft.Text("Standard Sheet",
+                                font_family = "HDText",
+                                size = 15,
+                                weight = "bold")
+                    ]
+                ),
+                content = std_tabs
+            ),
+            ft.Tab(
+                tab_content = ft.Row(
+                    [
+                        ft.Icon(ft.icons.DRIVE_FILE_RENAME_OUTLINE_SHARP),
+                        ft.Text("Paint Area Sheet",
+                                font_family = "HDText",
+                                size = 15,
+                                weight = "bold")
+                    ]
+                ),
+                content = paint_table
+            ),
+            ft.Tab(
+                 tab_content = ft.Row(
+                    [
+                        ft.Icon(ft.icons.CALCULATE),
+                        ft.Text("MH Calculation",
+                                font_family = "HDText",
+                                size = 15,
+                                weight = "bold")
+                    ]
+                ),
+                content = mc.mh_calc(page))
+        ]
+    )
+    
+    return ft.Column(
+        expand = True,
+        alignment = ft.MainAxisAlignment.CENTER,
+        controls = [top_menu]
+    )
