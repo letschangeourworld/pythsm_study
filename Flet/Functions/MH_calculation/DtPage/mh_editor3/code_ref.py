4a84e087c6fb5cb4d71ba021a6cac00e6ba02dcb
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-11-02 03:00:56 +0900

    Update code_ref.py

---
+++ Flet/Functions/MH_calculation/DtPage/mh_editor3/code_ref.py
@@ -0,0 +1,26 @@
+import flet as ft
+
+def code_ref_txt(keys, values):
+    return ft.Card(
+        ft.Container(
+            ft.Column(
+                [
+                    ft.Row(
+                        [
+                            ft.Text(
+                                f"Code {str(value)} : {key}",
+                                weight = "bold",
+                                font_family = "HDText",
+                                text_align = ft.TextAlign.CENTER,
+                                size = 13
+                            )
+                        ]
+                    ) for key, value in zip(keys, values)
+                ]
+            ),
+            alignment = ft.MainAxisAlignment.START
+        ),
+        elevation = 5
+    )
+    
+    
