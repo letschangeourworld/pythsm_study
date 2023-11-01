86679f3c2f3e1f915fd0dc5742835c40a8a780a7
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-11-01 21:29:43 +0900

    Update dataframe.py

---
+++ Flet/Functions/MH_calculation/DtPage/mh_editor3/dataframe.py
@@ -0,0 +1,33 @@
+import flet as ft
+import pandas as pd
+
+def col_name(df: pd.DataFrame) -> list:
+    return [
+        ft.DataColumn(
+            ft.Text(col.upper(),
+                    color = ft.colors.BLACK,
+                    weight = "bold",
+                    text_align = ft.TextAlign.CENTER,
+                    font_family = "HDText"
+            )
+        ) for col in df.columns
+    ]
+
+def rows(df: pd.DataFrame) -> list:
+    rows = []
+    for tp in df.itertuples():
+        rows.append(
+            ft.DataRow(
+                cells = [
+                    ft.DataCell(
+                        ft.Text(tp[col_no],
+                                color = ft.colors.BLACK87,
+                                weight = "bold",
+                                text_align = ft.TextAlign.RIGHT,
+                                font_family = "HDText"
+                        )
+                    ) for col_no in range(1,len(df.columns)+1)
+                ]
+            )
+        )
+    return rows
