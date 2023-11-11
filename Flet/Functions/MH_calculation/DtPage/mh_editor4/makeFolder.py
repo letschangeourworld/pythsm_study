5af86da02bdc892e88b26cf922bba7c8295b0624
Philip <50024239+letschangeourworld@users.noreply.github.com>
2023-11-11 21:55:09 +0900

    Update makeFolder.py

--- Flet/Functions/MH_calculation/DtPage/mh_editor4/makeFolder.py
+++ Flet/Functions/MH_calculation/DtPage/mh_editor4/makeFolder.py
@@ -5,8 +5,9 @@ def createFolder(directory):
     try:
         if not os.path.exists(directory):
             os.makedirs(directory)
+            
     except OSError:
-            print ('Error: Creating directory. ' +  directory) 
+        print('Error: Creating directory. ' +  directory) 
     
 
 createFolder('/aaa')
