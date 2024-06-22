c435e791968c1ffea28509afd007aa07decc7a40
Philip <50024239+letschangeourworld@users.noreply.github.com>
2024-06-22 09:12:29 +0900

    Update Packet.cpp

--- PLC/Packet.cpp
+++ PLC/Packet.cpp
@@ -18,31 +18,31 @@
         public ushort wSubCommand;    // Sub Command
 
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
-        public byte[] byDeviceAddr;   // 선두 Device Addr
-        public byte byDeviceCode;     // Device 코드
-        public ushort wCount;         // 읽기/쓰기 점수
+        public byte[] byDeviceAddr; // 선두 Device Addr
+        public byte byDeviceCode;   // Device 코드
+        public ushort wCount;        // 읽기/쓰기 점수
     };
 
 
 3E_BIN_REQUEST_COMMAND _Request_Header = new _3E_BIN_REQUEST_COMMAND();
             _Request_Header.byDeviceAddr = new byte[3];
 
-            int dwTemp = 0x64;   // 주소
+            int dwTemp = 0x64; // 주소
 
-	        _Request_Header.wSubHeader			= 0x0050;// 바이너리 50 Subheader			
-	        _Request_Header.byNetworkNo			= 0x00;				
-	        _Request_Header.byPlcNo				= 0xFF;				
-	        _Request_Header.wModuleIONo			= 0x03FF;			
-	        _Request_Header.byModuleStateNo		= 0x00;				
-	        _Request_Header.wDataLength			= 0x000C;			
-	        _Request_Header.wCpuTimer			= 0x0010;			
-	        _Request_Header.wCommand			= 0x0401;				// read command '0401';		
-	        _Request_Header.wSubCommand			= 0x0001;			
-	        _Request_Header.byDeviceAddr[0]		= (byte)(dwTemp		& 0x000000FF);
-            _Request_Header.byDeviceAddr[1]     = (byte)(dwTemp >> 8 & 0x000000FF);
-            _Request_Header.byDeviceAddr[2]     = (byte)(dwTemp >> 16 & 0x000000FF);
-            _Request_Header.byDeviceCode        = 0xC2; // T
-            _Request_Header.wCount              = (ushort)3; // 읽을 수량
+	        _Request_Header.wSubHeader	= 0x0050;// 바이너리 50 Subheader			
+	        _Request_Header.byNetworkNo	= 0x00;				
+	        _Request_Header.byPlcNo		= 0xFF;				
+	        _Request_Header.wModuleIONo	= 0x03FF;			
+	        _Request_Header.byModuleStateNo	= 0x00;				
+	        _Request_Header.wDataLength		= 0x000C;			
+	        _Request_Header.wCpuTimer		= 0x0010;			
+	        _Request_Header.wCommand			= 0x0401;// read command '0401';		
+	        _Request_Header.wSubCommand		= 0x0001;			
+	        _Request_Header.byDeviceAddr[0]	= (byte)(dwTemp	& 0x000000FF);
+            _Request_Header.byDeviceAddr[1]  = (byte)(dwTemp >> 8 & 0x000000FF);
+            _Request_Header.byDeviceAddr[2]  = (byte)(dwTemp >> 16 & 0x000000FF);
+            _Request_Header.byDeviceCode     = 0xC2; // T
+            _Request_Header.wCount  = (ushort)3; // 읽을 수량
 
             byte[] buffer = new byte[Marshal.SizeOf(_Request_Header)];
 
